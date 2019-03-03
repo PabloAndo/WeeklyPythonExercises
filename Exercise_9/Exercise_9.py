7130527fbb5bbaae8e78eb65d5ab0b8e2a6b852b
Pablo Ando - Laptop <pabloando99@gmail.com>
2019-03-03 20:10:37 +0100

    Exercise 9 complete. All tests passed.

--- Exercise_9/Exercise_9.py
+++ Exercise_9/Exercise_9.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
+letters = 'abcde'
+numbers = [1, 2, 3, 4, 5]
+symbols = '!@#$%'
 
-def multiziperator():
-    pass
+
+def multiziperator(*args):
+    for one_index in zip(*args):
+        for one_element in one_index:
+            yield one_element
 
 
 if __name__ == "__main__":
-    multiziperator()
+    for one_item in multiziperator(letters, numbers, symbols):
+        print(one_item)
